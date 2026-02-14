d337cfcf111690991c91283f7a5a53e05fb97438
Martin Kolev <yoanaamazon@gmail.com>
2026-02-14 19:24:22 +0200

    Add files via upload

---
+++ CarService.Tests/CarMockedData.cs
@@ -0,0 +1,14 @@
+﻿using CarService.Models.Dto;
+
+namespace CarService.Tests.MockData
+{
+    internal class CarMockedData
+    {
+        public static List<Car> Cars = new List<Car>
+        {
+        new Car { Id = Guid.NewGuid(), Model = "Toyota Camry", Year = 2020 },
+        new Car { Id = Guid.NewGuid(), Model = "Honda Accord", Year = 2019 },
+        new Car { Id = Guid.NewGuid(), Model = "Ford Mustang", Year = 2021 }
+            };
+    }
+}
